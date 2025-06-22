d868aa64372cdc042fe961a0e87574c8b3457552
juamorrio <jmorenorios5@gmail.com>
2025-06-22 10:37:24 +0200

    Fixed little error in airport

--- src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
@@ -63,11 +63,11 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
 
 		Airport a = this.repository.findAirportByIataCode(airport.getIATAcode());
-		if (a.getId() != airport.getId())
+		if (a != null && a.getId() != airport.getId())
 			super.state(false, "IATAcode", "acme.validation.confirmation.message.aiport.IATAcode");
 
 		Airport a2 = this.repository.findAirportByEmail(airport.getEmail());
-		if (a2.getId() != airport.getId())
+		if (a2 != null && a2.getId() != airport.getId())
 			super.state(false, "email", "acme.validation.confirmation.message.aiport.email");
 	}
 
