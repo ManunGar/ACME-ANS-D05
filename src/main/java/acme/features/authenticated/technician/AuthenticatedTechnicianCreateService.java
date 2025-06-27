269bc5a4b21f15f41bb0fe536c7e119ae95cf032
alerumar <alejandrorumar@gmail.com>
2025-06-27 11:40:49 +0200

    D03-I5-17 done, fixed every error

---
+++ src/main/java/acme/features/authenticated/technician/AuthenticatedTechnicianCreateService.java
@@ -0,0 +1,80 @@
+
+package acme.features.authenticated.technician;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Authenticated;
+import acme.client.components.principals.UserAccount;
+import acme.client.helpers.PrincipalHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.realms.Technician;
+
+@GuiService
+public class AuthenticatedTechnicianCreateService extends AbstractGuiService<Authenticated, Technician> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedTechnicianRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = !super.getRequest().getPrincipal().hasRealmOfType(Technician.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Technician technician;
+		int userAccountId;
+		UserAccount userAccount;
+
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+		userAccount = this.repository.findUserAccountById(userAccountId);
+
+		technician = new Technician();
+		technician.setUserAccount(userAccount);
+
+		super.getBuffer().addData(technician);
+	}
+
+	@Override
+	public void bind(final Technician technician) {
+		super.bindObject(technician, "licenseNumber", "phoneNumber", "specialisation", "passedAnnualHealthTest", "yearsOfExperience", "certifications");
+	}
+
+	@Override
+	public void validate(final Technician technician) {
+		;
+	}
+
+	@Override
+	public void perform(final Technician technician) {
+
+		this.repository.save(technician);
+	}
+
+	@Override
+	public void unbind(final Technician technician) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(technician, "licenseNumber", "phoneNumber", "specialisation", "passedAnnualHealthTest", "yearsOfExperience", "certifications");
+
+		super.getResponse().addData(dataset);
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals("POST"))
+			PrincipalHelper.handleUpdate();
+	}
+
+}
