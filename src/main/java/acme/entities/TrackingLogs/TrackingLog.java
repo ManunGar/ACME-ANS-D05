debe8f5b02bff2c45909804ad3e29d9aabe75235
kawayypro <juanantoniorrl2004@gmail.com>
2025-06-28 14:42:24 +0200

    quick fix

--- src/main/java/acme/entities/TrackingLogs/TrackingLog.java
+++ src/main/java/acme/entities/TrackingLogs/TrackingLog.java
@@ -74,7 +74,7 @@ public class TrackingLog extends AbstractEntity {
 	/*
 	 * The `createdMoment` attribute is used to chronologically
 	 * sort the different trackingLogs in the database and
-	 * toperform all checks and validations, whereas
+	 * to perform all checks and validations, whereas
 	 * `lastUpdateMoment` is the attribute that informs the user
 	 * about the last time this trackingLog was modified.
 	 */
